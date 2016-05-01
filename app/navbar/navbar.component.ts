aad9db0535053aa13ffd260109397cb964772962
olivierl <olivier.lambeaux@gmail.com>
2016-05-01 10:44:50 +0200

    highlight current menu

--- app/navbar/navbar.component.ts
+++ app/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import {Component} from 'angular2/core';
-import {ROUTER_DIRECTIVES} from 'angular2/router';
+import {ROUTER_DIRECTIVES, Router} from 'angular2/router';
 
 @Component({
   selector: 'navbar',
@@ -7,5 +7,11 @@ import {ROUTER_DIRECTIVES} from 'angular2/router';
   directives: [ROUTER_DIRECTIVES]
 })
 export class NavbarComponent {
-
+  constructor(private _router: Router){
+  }
+ 
+  isCurrentRoute(route){
+    var instruction = this._router.generate(route);
+    return this._router.isRouteActive(instruction);
+  }
 }
