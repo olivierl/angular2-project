310ba4a3377189e0b9faea4693c064e9273424c9
olivierl <olivier.lambeaux@gmail.com>
2016-05-14 10:34:39 +0200

    Add delete user

--- app/users/user.service.ts
+++ app/users/user.service.ts
@@ -25,6 +25,10 @@ export class UserService {
     return this._http.put(this.getUserUrl(user.id), JSON.stringify(user)).map(res => res.json());
   }
   
+  deleteUser(userId) {
+    return this._http.delete(this.getUserUrl(userId)).map(res => res.json());
+  }
+  
   private getUserUrl(userId){
 		return this._baseUrl + '/' + userId;
 	}
--- app/users/users.component.ts
+++ app/users/users.component.ts
@@ -19,4 +19,23 @@ export class UsersComponent implements OnInit {
     this._userService.getUsers()
       .subscribe(users => this.users = users);
   }
+
+  deleteUser(user) {
+    if (confirm('Are you sure you want to delete ' + user.name + '?')) {
+      var index = this.users.indexOf(user)
+      // Here, with the splice method, we remove 1 object
+      // at the given index.
+      this.users.splice(index, 1);
+
+      this._userService.deleteUser(user.id)
+        .subscribe(null,
+        err => {
+          alert('Could not delete the user.');
+          // Revert the view back to its original state
+          // by putting the user object at the index
+          // it used to be.
+          this.users.splice(index, 0, user);
+        });
+    }
+  }
 }
